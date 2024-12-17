b7a069bbdee4f0cb768c50e98ee7949c9b90ad5d
김준하 <junhakjh@gmail.com>
2024-12-17 22:44:59 +0900

    ✨ feat: 사이드바 디자인 수정

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -14,10 +14,10 @@ const Sidebar: React.FC<PSidebar> = ({ sections }) => {
   const [activeItem, setActiveItem] = useState<string>('');
 
   return (
-    <div className='w-64 rounded-lg bg-white p-4'>
+    <div className='flex flex-col gap-8 w-64 rounded-lg bg-white py-6 px-2'>
       {sections.map((section) => (
-        <div key={section.title} className='mb-4'>
-          <h3 className='mb-2 px-3 text-body2 font-semibold text-primary'>{section.title}</h3>
+        <div key={section.title}>
+          <h3 className='mb-2 px-4 text-detail font-semibold text-primary'>{section.title}</h3>
           {section.items.map((item) => (
             <SidebarItem key={item} label={item} isActive={activeItem === item} onClick={() => setActiveItem(item)} />
           ))}
