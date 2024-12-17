a28d70ca5fa1829c30db9b0f3b9fd94beb534962
wls9804 <wls9804@naver.com>
2024-12-17 19:43:59 +0900

    :sparkles: feat: Navbar 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
+const App: React.FC = () => {
   return (
-    <div className='text-heading1'>
-      <div>안녕하세요!</div>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/jobs" element={<div>채용공고 페이지</div>} />
+        <Route path="/portfolio" element={<div>포트폴리오 페이지</div>} />
+        <Route path="/community" element={<div>커뮤니티 페이지</div>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
