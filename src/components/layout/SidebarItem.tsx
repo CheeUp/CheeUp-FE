cd68312c32b6261aa4853adf67683f4e76cdd18c
10hajin15@gmail.com <10hajin15@gmail.com>
2024-12-17 19:40:37 +0900

    🐛 fix: 불필요한 코드 삭제

--- src/components/layout/SidebarItem.tsx
+++ src/components/layout/SidebarItem.tsx
@@ -10,7 +10,7 @@ const SidebarItem: React.FC<PSidebarItem> = ({ label, isActive, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className={`group cursor-pointer rounded-md px-3 py-2 text-body2 transition-all ${isActive ? 'bg-primary font-semibold text-white' : 'hover:bg-primary-light hover:text-white'} `}
+      className={`cursor-pointer rounded-md px-3 py-2 text-body2 transition-all ${isActive ? 'bg-primary font-semibold text-white' : 'hover:bg-primary-light hover:text-white'} `}
     >
       <span>{label}</span>
     </div>
