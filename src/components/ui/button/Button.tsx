9098b0f52ecd32371272eca75e4d79f639034896
okxooxoo <okxooxoo@naver.com>
2024-12-18 13:50:34 +0900

    🎨 design(SCRUM-113): Button hover 시 투명도 변경

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,8 +1,9 @@
-import { PropsWithChildren } from 'react';
+import { ReactNode } from 'react';
 
-interface PButton extends PropsWithChildren {
-  type?: 'filled' | 'outlined' | 'delete-filled' | 'delete-outlined' | 'cancle-outlined';
+interface PButton {
+  children: ReactNode;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  type?: 'filled' | 'outlined' | 'delete-filled' | 'delete-outlined' | 'cancel-outlined';
   className?: string;
 }
 
@@ -11,7 +12,7 @@ export const Button = ({ children, type = 'filled', onClick, className = '' }: P
 
   return (
     <button
-      className={`cursor-pointer rounded-sm border px-4 py-2 text-body2 ${borderColor} ${bgColor} ${textColor} ${className} ease duration-300 hover:scale-110 active:scale-100`}
+      className={`cursor-pointer rounded-sm border px-4 py-2 text-body2 ${borderColor} ${bgColor} ${textColor} ${className} hover:opacity-90`}
       onClick={onClick}
     >
       {children}
@@ -24,5 +25,5 @@ const buttonStyles = {
   outlined: { borderColor: 'border-primary', bgColor: 'bg-white', textColor: 'text-primary' },
   'delete-filled': { borderColor: 'border-action', bgColor: 'bg-action', textColor: 'text-white' },
   'delete-outlined': { borderColor: 'border-action', bgColor: 'bg-white', textColor: 'text-action' },
-  'cancle-outlined': { borderColor: 'border-input', bgColor: 'bg-white', textColor: 'text-disabled' },
+  'cancel-outlined': { borderColor: 'border-input', bgColor: 'bg-white', textColor: 'text-disabled' },
 };
