cdf31c8758170b8ee1ddeb3278ba6333d5a8e8f8
김준하 <junhakjh@gmail.com>
2024-12-18 21:16:16 +0900

    ✨ feat: 헤더 전역 상단에 고정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/layout/Header';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<h1>Home</h1>} />
       </Routes>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import Logo from '../Logo';
-import { Button } from '../ui/button/Button';
+import Button from '../ui/button/Button';
 import { Link, NavLink } from 'react-router-dom';
-const Navbar: React.FC = () => {
+const Header: React.FC = () => {
   return (
-    <header className='bg-background p-6 shadow-md'>
+    <header className='sticky top-0 z-50 bg-background px-14 py-6 shadow-md'>
       {/* 전체 */}
-      <div className='flex items-center justify-between px-[90px]'>
+      <div className='flex items-center justify-between'>
         {/* 로고와 메뉴 */}
-        <div className='flex items-center gap-[140px]'>
+        <div className='flex items-center gap-20'>
           {/* 로고 */}
           <Link to='/' className='flex items-center'>
             <Logo className='h-8' />
           </Link>
           {/* 메뉴 */}
           <nav>
-            <ul className='flex gap-[114px] text-black'>
+            <ul className='flex gap-16 text-black'>
               <li>
                 <NavLink
                   to='/jobs'
@@ -44,15 +44,12 @@ const Navbar: React.FC = () => {
           </nav>
         </div>
         <div className='flex gap-4'>
-        {/* 로그인/회원가입 */}
-          <Button  onClick={() => console.log('버튼 클릭')}>
-            로그인
-          </Button>
+          {/* 로그인/회원가입 */}
+          <Button onClick={() => {}}>로그인</Button>
         </div>
       </div>
     </header>
   );
 };
 
-export default Navbar;
-
+export default Header;
