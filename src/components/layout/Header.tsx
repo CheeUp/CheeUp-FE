452a48d5e1ec763e1b0ae91cb50177fbd1de2bb1
GuKBABjOa <128679235+GuKBABjOa@users.noreply.github.com>
2024-12-23 16:48:17 +0900

    :recycle: refactor(SCRUM-206): Navigation-Bar-Fix (#8)
    * :recycle: refactor: 중복코드 개선 + 로그인 버튼 구현
    
    * :lipstick: style: goToLogin -> onLoginClick
    
    * :lipstick: style: onLoginClick -> handleLoginClick

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,55 +1,60 @@
 import React from 'react';
 import Logo from '../Logo';
-import Button from '../ui/button/Button';
-import { Link, NavLink } from 'react-router-dom';
-const Header: React.FC = () => {
+import  Button  from '../ui/button/Button';
+import { Link, NavLink,useNavigate } from 'react-router-dom';
+
+const navItems = [
+  { title: '채용공고', to: '/jobs' },
+  { title: '포트폴리오', to: '/portfolio' },
+  { title: '커뮤니티', to: '/community' },
+];
+
+const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate('/login');
+  }
+
   return (
+<<<<<<< HEAD
+    <header className='bg-background p-6'>
+      <div className='flex items-center justify-between px-[90px]'>
+=======
     <header className='sticky top-0 z-50 border-b border-border bg-white px-14 py-3'>
       {/* 전체 */}
       <div className='flex items-center justify-between'>
+>>>>>>> 31bd2c832104ef9c97b0d7ab280497cb9e4fdd44
         {/* 로고와 메뉴 */}
-        <div className='flex items-center gap-20'>
+        <div className='flex items-center gap-[140px]'>
           {/* 로고 */}
           <Link to='/' className='flex items-center'>
             <Logo className='h-8' />
           </Link>
           {/* 메뉴 */}
           <nav>
-            <ul className='flex gap-16 text-black'>
-              <li>
-                <NavLink
-                  to='/jobs'
-                  className={({ isActive }) => (isActive ? 'text-primary' : 'text-black hover:text-primary-light')}
-                >
-                  채용공고
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to='/portfolio'
-                  className={({ isActive }) => (isActive ? 'text-primary' : 'text-black hover:text-primary-light')}
-                >
-                  포트폴리오
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to='/community'
-                  className={({ isActive }) => (isActive ? 'text-primary' : 'text-black hover:text-primary-light')}
-                >
-                  커뮤니티
-                </NavLink>
-              </li>
+            <ul className='flex gap-[114px] text-black'>
+              {navItems.map((item) => (
+                <li key={item.to}>
+                  <NavLink
+                    to={item.to}
+                    className={({ isActive }) =>
+                      isActive ? 'text-primary' : 'text-black hover:text-primary-light'
+                    }
+                  >
+                    {item.title}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
+        {/* 로그인/회원가입 */}
         <div className='flex gap-4'>
-          {/* 로그인/회원가입 */}
-          <Button onClick={() => {}}>로그인</Button>
+          <Button onClick={handleLoginClick}>로그인</Button>
         </div>
       </div>
     </header>
   );
 };
 
-export default Header;
+export default Navbar;
