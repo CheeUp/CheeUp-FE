8a9c7db003850643867bc60ec14a66cda3007019
김준하 <junhakjh@gmail.com>
2024-12-25 22:38:43 +0900

    🎨 design(SCRUM-268): 취업공고 미리보기 뱃지 텍스트 가운데 정렬 적용

--- src/components/ui/badge/RecruitStateBadge.tsx
+++ src/components/ui/badge/RecruitStateBadge.tsx
@@ -5,7 +5,7 @@ interface PRecruitStateBadge {
 const RecruitStateBadge: React.FC<PRecruitStateBadge> = ({ isStart }) => {
   return (
     <div
-      className={`h-4 rounded-[3px] px-0.5 text-center text-detail text-white ${isStart ? 'bg-complementary-lightcrimson' : 'bg-complementary-mediumpurple'}`}
+      className={`flex h-4 items-center rounded-[3px] px-1 text-center text-detail text-white ${isStart ? 'bg-complementary-lightcrimson' : 'bg-complementary-mediumpurple'}`}
     >
       {isStart ? 'start' : 'end'}
     </div>
