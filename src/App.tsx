dc58c889a15d184e1bf51770bf3e7a4373739cee
10hajin15@gmail.com <10hajin15@gmail.com>
2024-12-31 15:58:32 +0900

    ✨ feat: 새로운 글 작성 경로 /community/new 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import RecruitPage from '@/components/pages/recruit';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PortfolioPage from '@/components/pages/portfolio';
 import NewPortfolioPage from '@/components/pages/portfolio/new';
+import NewPostPage from '@/components/pages/community/post';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Route path='/recruit' element={<RecruitPage />} />
         <Route path='/portfolio' element={<PortfolioPage />} />
         <Route path='/portfolio/new' element={<NewPortfolioPage />} />
+        <Route path='/community/new' element={<NewPostPage />} />
       </Routes>
     </BrowserRouter>
   );
