286ee237c965e05bb3e4c91df5cd064dd5e40cf7
김준하 <junhakjh@gmail.com>
2025-01-03 17:51:16 +0900

    ✨ feat: DatePicker 시간 추가

--- src/util/date.ts
+++ src/util/date.ts
@@ -19,3 +19,24 @@ export const getDDay = (date: Date): string => {
     return '종료 ' + Math.floor(diff / (1000 * 60 * 60 * 24)) + '일 전';
   }
 };
+
+export const formatTime = (input: string): string => {
+  const sanitizedInput = input.replace(/[^0-9]/g, '');
+
+  let hours = sanitizedInput.slice(0, 2);
+  let minutes = sanitizedInput.slice(2, 4);
+
+  if (hours) {
+    const numericHours = Math.min(23, parseInt(hours, 10));
+    hours = numericHours.toString().padStart(2, '0');
+  }
+
+  if (minutes) {
+    const numericMinutes = Math.min(59, parseInt(minutes, 10));
+    minutes = numericMinutes.toString().padStart(2, '0');
+  } else {
+    minutes = '00';
+  }
+
+  return `${hours}:${minutes}`;
+};
